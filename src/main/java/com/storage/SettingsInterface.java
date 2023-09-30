b48660dabeff5480a27d9835c8a966ce905ef297
Magistrat <nisraton@gmail.com>
2023-10-01 01:29:31 +0300

    [feature] create interface with base and register url

---
+++ src/main/java/com/storage/SettingsInterface.java
@@ -0,0 +1,7 @@
+package com.storage;
+
+public interface SettingsInterface {
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/";
+
+    public static final String REGISTER_URL = "/api/auth/register";
+}
