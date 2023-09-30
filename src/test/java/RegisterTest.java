8ae7a2221e8283d97ee390ee0ccac7c3032f0ed8
Magistrat <nisraton@gmail.com>
2023-10-01 02:44:53 +0300

    [feature] draft first test (createUniqueUserTest)

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,17 +1,25 @@
+import com.storage.pojo.register.RegisterPositivePojo;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static com.storage.Base.generateTestData;
-import static com.storage.Base.generateTestDataEmail;
-import static com.storage.SettingsInterface.BASE_URL;
+import static com.storage.GenerationData.generateTestData;
+import static com.storage.GenerationData.generateTestDataEmail;
+import static com.storage.RestAssuredBaseMethods.checkResponseStatusCode;
+import static com.storage.RestAssuredBaseMethods.sendByPost;
+import static com.storage.SettingsInterface.*;
 
 
 public class RegisterTest {
     private String generatedTestEmail;
     private String generatedTestPassword;
     private String generatedTestName;
+    private RegisterPositivePojo positiveRegister;
+
+    private Response response;
 
     @Before
     public void setUp(){
@@ -20,7 +28,27 @@ public class RegisterTest {
         generatedTestEmail = generateTestDataEmail();
         generatedTestPassword = generateTestData("password");
         generatedTestName = generateTestData("name");
+
+        positiveRegister = new RegisterPositivePojo(
+//                generateTestDataEmail(),
+//                generateTestData("password"),
+//                generateTestData("name")
+                generatedTestEmail,
+                generatedTestPassword,
+                generatedTestName
+        );
     }
 
+    @Test
+    @DisplayName("Создать уникального пользователя")
+    public void createUniqueUserTest(){
 
+        Response response = sendByPost(REGISTER_USER_URL, positiveRegister);
+        checkResponseStatusCode(response, SUCCESS_STATUS_CODE);
+    }
+
+    @After
+    public void deleteUser(){
+        System.out.println();
+    }
 }
