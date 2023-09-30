01ef2a8612877dee5bedccdcd3225f9e1cc1ccde
Magistrat <nisraton@gmail.com>
2023-10-01 02:40:09 +0300

    [feature] add SUCCESS_STATUS_CODE

--- src/main/java/com/storage/SettingsInterface.java
+++ src/main/java/com/storage/SettingsInterface.java
@@ -8,4 +8,6 @@ public interface SettingsInterface {
 
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String APPLICATION_JSON = "application/json";
+
+    public static final int SUCCESS_STATUS_CODE = 200;
 }
